9c8fd4f87846485758523ed0cc3fbd2fbc00cbdd
syafuan1234 <amrcosec.system@gmail.com>
2025-08-22 13:23:02 +0800

    Fix document error

--- companies/urls.py
+++ companies/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from django.http import HttpResponse
 from . import views
-from .views import download_pdf
 
 urlpatterns = [
     path('', lambda request: HttpResponse("✅ Welcome to AMRCOSEC System! Your system is running.")),
@@ -9,6 +8,4 @@ urlpatterns = [
     path('generate-doc/<int:company_id>/<int:template_id>/', views.generate_company_doc, name='generate_company_doc'),
     path('choose-template/<int:company_id>/', views.choose_template, name='choose_template'),
     path('generate-doc/<int:company_id>/<int:template_id>/<str:director_id>/', views.generate_company_doc, name='generate_company_doc_with_director'),
-    path('companies/<int:company_id>/template/<int:template_id>/pdf/', download_pdf, name='download_pdf'),
-
 ]
